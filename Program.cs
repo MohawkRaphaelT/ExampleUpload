f29f317772a680c6ec277865f89617e1e8bbebda
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2024-06-14 11:46:24 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExampleUpload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
